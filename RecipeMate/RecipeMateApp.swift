a07b63565338bf4766c89518a875553890dca192
Um, Jungmin <jum9@gatech.edu>
2025-05-24 21:21:46 -0400

    Initial Commit

---
+++ RecipeMate/RecipeMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeMateApp.swift
+//  RecipeMate
+//
+//  Created by mina um on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipeMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
